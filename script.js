f457cf50c8d92979f02a99b1b90bce531d088b5f
shatakshi <shatakshi.sharma@charpixel.com>
2021-05-01 20:08:11 +0530

    radio button functionality improved

--- script.js
+++ script.js
@@ -2,22 +2,48 @@ const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
-console.log(messageInput,"messageInput")
 
 const name = prompt('What is your name?')
 appendMessage('You joined')
 socket.emit('new-user', name)
 
+const currentUser = name;
 
 socket.on('chat-message', data => {
   appendMessage(`${data.name}: ${data.message}`)
 })
 
-socket.on('user-connected', data=> {
+var addedUsers = {}
+
+socket.on('new-user-connected', data => {
+
+  const {id, users} = data
+
+  delete users[id]
+ 
+
+  for(var key in users) {
+    addedUsers[key] = 1
+    add_field(key, users[key]);
+  }
+
+})
+
+socket.on('user-connected', data => {
    
   appendMessage(`${data.name} connected`)
-  var usersArray = data.users
-  console.log(data.users,"users array")
+  
+  var usersObj = data.users
+
+    const {id} = data
+    for(var key in usersObj) {
+    
+      if(addedUsers[key] != 1 && usersObj[key] != currentUser){
+        addedUsers[key] = 1
+        add_field(key, usersObj[key]);
+      }    
+    }
+
 })
 
 socket.on('user-disconnected', name => {
@@ -27,57 +53,44 @@ socket.on('user-disconnected', name => {
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
   const message = messageInput.value
-  appendMessage(`You: ${message}`)
-  socket.emit('send-chat-message', message,selectedSocketId)
+  appendMessage(`You: ${message},${selectedSocketId}`)
+  
+  socket.emit('send-chat-message', {message,selectedSocketId})
   messageInput.value = ''
 })
 
 function appendMessage(message) {
   const messageElement = document.createElement('div')
-  //console.log(messageElement,"*****")
+  
   messageElement.innerText = message
   messageContainer.append(messageElement)
-  //console.log(messageContainer,"*****")
+ 
 }
 
 var selectedSocketId = "";
-// var obj = {
-// 	"1234": "Saurabh",
-//     "5678": "Shatakshi",
-// 	"5642": "Ashu"
-// }
-
-
-for(var key in usersArray) {
-	add_field(key, usersArray[key]);
-}
-
 
 function onChangeFunction() {
 selectedSocketId = document.querySelector('input[name="socket"]:checked').value;
-	alert(selectedSocketId);
+
 }
 
 
 function add_field(socketId, name){
-                var x = document.getElementById("socket-form");
-                // create an input field to insert
-                var new_field = document.createElement("input");
-                // set input field data type to text
-                new_field.setAttribute("type", "radio");
-                // set input field name 
-                new_field.setAttribute("name", "socket");
-                // select last position to insert element before it
-                var pos = x.childElementCount;
-				new_field.setAttribute("value", socketId);
-				new_field.onchange = onChangeFunction;
-                // insert element
-                x.insertBefore(new_field, x.childNodes[pos]);
+       var x = document.getElementById("socket-form");// create an input field to insert
+       var new_field = document.createElement("input"); // set input field data type to text
+       new_field.setAttribute("type", "radio");  // set input field name 
+       new_field.setAttribute("name", "socket"); // select last position to insert element before it
+       var pos = x.childElementCount;
+       new_field.setAttribute("value", socketId);
+       new_field.onchange = onChangeFunction; // insert element
+       x.insertBefore(new_field, x.childNodes[pos]);
+          
+
+       var label_field = document.createElement("label");
+       label_field.innerHTML = name;
+       x.insertBefore(label_field, x.childNodes[pos+1]);        
+
                 
-                var label_field = document.createElement("label");
-                label_field.innerHTML = name;
-                x.insertBefore(label_field, x.childNodes[pos+1]);
-                
-            }
+ }
     
-//socket.emit('send-chat-message', selectedSocketId)            
+        
--- server.js
+++ server.js
@@ -3,20 +3,17 @@ const io = require('socket.io')(3000)
 users = {}
 
 io.on('connection', socket => {
- // console.log(socket.id,"i am socket obj",socket.name)
-  //console.log(socket)
   socket.on('new-user', name => {
-    //  userconnected ='true'
-    // console.log(userconnected,"***")
-  
+
     users[socket.id] = name
-    socket.broadcast.emit('user-connected', {name,users})
+    socket.emit('new-user-connected', {id: socket.id, name, users})
+    socket.broadcast.emit('user-connected', {id: socket.id,name,users})
   })
-  //console.log(userconnected,"===")
+  
 
   socket.on('send-chat-message', data=> {
-    socket.to(data.selectedSocketId).emit('chat-message', { message: data.message, name: users[socket.id]});
-    //socket.emit('chat-message', { message: message, name: users[socket.id],id:socket.id})
+
+    io.to(data.selectedSocketId).emit('chat-message', { message: data.message, name: users[socket.id]});
   })
 
   socket.on('disconnect', () => {
@@ -24,10 +21,5 @@ io.on('connection', socket => {
     delete users[socket.id]
   })
 
+
 })
-//console.log(users,"______________________________")
-
-
-   //console.log(users)
-    // console.log(users,"*********")
-    // console.log(message,users[socket.id], socket.id,"))))")
